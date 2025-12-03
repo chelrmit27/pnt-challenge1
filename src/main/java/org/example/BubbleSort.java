3226558b33ef7dfa2f3867c2d2ef843d522a7530
Tam Tran <s3999159@rmit.edu.vn>
2025-12-03 09:36:41 +0700

    retry q3

--- src/main/java/org/example/BubbleSort.java
+++ src/main/java/org/example/BubbleSort.java
@@ -12,7 +12,6 @@ public class BubbleSort {
             swapped = false;
             for (int j = 0; j < n - 1 - i; j++) {
                 if (a[j] > a[j + 1]) {
-		    System.out.println("Swapping" + a[j] + "and" + a[j+1]);
 		    int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
